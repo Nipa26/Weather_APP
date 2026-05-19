06e8f6d7106983e33b66c85d707e551ebf496f4d
Nipa-Full-Stack-Developer <nipacse159@gmail.com>
2026-05-19 22:44:36 +0600

    tsconfig.json. server.ts, gitignore setup done

---
+++ backend/src/server.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const API_KEY = process.env.OPENWEATHER_API_KEY as string;
+
+type WeatherResponse = {
+  name: string;
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  weather: { description: string }[];
+};
+
+app.get("/weather/:city", async (req, res) => {
+  try {
+    const city = req.params.city;
+
+    const { data } = await axios.get<WeatherResponse>(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    );
+
+    res.json(data);
+  } catch (error: any) {
+    res.status(500).json({
+      message: "Failed to fetch weather",
+      error: error.response?.data || error.message,
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
